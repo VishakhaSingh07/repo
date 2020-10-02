b6de4fabe4d6ca3d5bb71466000c4039330f4971
VishakhaSingh07 <68512366+VishakhaSingh07@users.noreply.github.com>
2020-10-02 20:36:53 +0530

    Add files via upload

---
+++ mai_n.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r",stdin);
+	// freopen("output.txt", "w",stdout);
+	// #endif
+
+	int n,sum=0;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	for(int i=1;i<n;i++){
+		if((a[i-1]-a[i])>0){
+			sum=sum+(a[i-1]-a[i]);
+		}
+	}
+	cout<<sum;
+
+
+
+	return 0;
+}
